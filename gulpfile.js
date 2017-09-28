367e4fa98a896f6b00181fc703169bfc8b354dd9
Marek <marekslad@hotmail.com>
2017-09-28 16:59:11 +0200

    styling, artists, metadata

--- gulpfile.js
+++ gulpfile.js
@@ -17,9 +17,10 @@ var rename = require('gulp-rename');
 
 var Data = {};
 
-Handlebars.registerHelper('encodeURI', function (options) {
-  return options.fn(this).replace(/ /g,'_');
+Handlebars.registerHelper('fullName', function (person) {
+  return person.firstName + " " + person.lastName;
 });
+
 // uglify js and css
 gulp.task('useref', function () {
   return gulp.src('src/*.html')
@@ -34,12 +35,16 @@ gulp.task('clean:dist', function () {
 });
 // clean pages folder
 gulp.task('clean:pages', function () {
-  initList();
+  initList('pages');
   return del.sync('src/pages/*.*');
+});
+gulp.task('clean:artists', function () {
+  initList('artists');
+  return del.sync('src/artists/*.*');
 });
 // build page
 gulp.task('build', function (callback) {
-  runSequence('clean:dist', 'sass', ['regenerate', 'pages', 'useref'],
+  runSequence('clean:dist', 'sass', 'regenerate', 'regenerate_artists', ['pages', 'artists', 'useref'],
     callback
   );
 });
@@ -58,7 +63,7 @@ gulp.task('browser-sync', ['sass'], function () {
     notify: true
   });
   gulp.watch('src/scss/*.scss', ['sass']);
-  gulp.watch('src/templates/**/*.{hbs,md}', ['regenerate']);
+  gulp.watch('src/templates/**/*.{hbs,md}', ['regenerate', 'regenerate_artists']);
   gulp.watch('src/index.html').on('change', browserSync.reload);
 });
 // generate pages from md templates
@@ -84,17 +89,12 @@ gulp.task('generate_pages', function () {
             data.name = name;
             // add url to our meta data
             data.url = "pages/" + file.relative.replace(".md", ".html");
-
-
-            data.artistLinks = _.map(data.artists, function(el){
-              return Handlebars.Utils.escapeExpression(el);
-            });
             // save meta data into object outside stream
             Data.pages.unshift(data);
             console.log('Processed: ' + data.name);
             // replace file contents without meta data
             contents = contents.slice(index + 3, contents.length);
-            data.excerpt = contents.slice(0,139);
+            data.excerpt = contents.slice(0, 139);
             file.contents = new Buffer(contents, "utf-8");
           }
         }))
@@ -125,8 +125,75 @@ gulp.task('generate_pages', function () {
 // copy generated pages to dist
 gulp.task('pages', function () {
   gulp.src("src/pages/**.*")
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist/pages'));
 });
+gulp.task('generate_artists', function () {
+  // read the template from page.hbs
+  return gulp.src('src/templates/artist.hbs')
+    .pipe(tap(function (file) {
+      // file is page.hbs so generate template from file
+      var template = Handlebars.compile(file.contents.toString());
+
+      // now read all the pages from the pages directory
+      return gulp.src('src/templates/artists/**.md')
+        .pipe(tap(function (file) {
+          // use path library to get file name
+          var name = path.basename(file.path, ".md");
+          // read meta data contents
+          var contents = file.contents.toString();
+          var index = contents.indexOf("---");
+          // metadata found
+          if (index !== -1) {
+            var data = JSON.parse(contents.slice(0, index));
+            // add name to meta data for lookup
+            data.name = name;
+            // add url to our meta data
+            data.url = "pages/" + file.relative.replace(".md", ".html");
+            // save meta data into object outside stream
+            Data.artists.unshift(data);
+            console.log('Processed: ' + data.name);
+            // replace file contents without meta data
+            contents = contents.slice(index + 3, contents.length);
+            // data.excerpt = contents.slice(0, 139);
+            file.contents = new Buffer(contents, "utf-8");
+          }
+        }))
+        // convert from markdown
+        .pipe(markdown())
+        .pipe(tap(function (file) {
+          var name = path.basename(file.path, ".html");
+          // file is the converted HTML from the markdown
+          // set the contents to the contents property on data
+          var data = _.findWhere(Data.artists, {
+            name: name
+          });
+          if (data !== undefined) {
+            data.contents = file.contents.toString();
+            // we will pass data to the Handlebars template to create the actual HTML to use
+            var html = template(data);
+            // replace the file contents with the new HTML created from the Handlebars template + data object that contains the HTML made from the markdown conversion
+            file.contents = new Buffer(html, "utf-8");
+          }
+        }))
+        .pipe(gulp.dest('src/artists'));
+    }))
+
+    .on('end', function () {
+      console.log('Completed generating artists!');
+    });
+});
+// copy generated pages to dist
+gulp.task('artists', function () {
+  gulp.src("src/artists/**.*")
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist/artists'));
+});
+
 // generate index.html with list of pages
 gulp.task('homepage', function () {
   return gulp.src("src/templates/index.hbs")
@@ -153,9 +220,16 @@ gulp.task('regenerate', function (callback) {
     callback
   );
 });
+// regenerate pages and index.html
+gulp.task('regenerate_artists', function (callback) {
+  runSequence('clean:artists',
+    'generate_artists',
+    callback
+  );
+});
 // clear pages lsit before regeneration
-function initList() {
-  Data.pages = [];
+function initList(list) {
+  Data[list] = [];
 }
 // default task
 gulp.task('default', ['browser-sync']);
